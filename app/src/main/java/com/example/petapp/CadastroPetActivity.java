e2d9a9da68d11c85cfc91e418e7d664c892f7eac
VitorSilvha <vitor.s.conceicao6@aluno.senai.br>
2024-10-24 21:15:14 -0300

    Vinculando o banco de dados ao CadastroPet

--- app/src/main/java/com/example/petapp/CadastroPetActivity.java
+++ app/src/main/java/com/example/petapp/CadastroPetActivity.java
@@ -1,55 +1,56 @@
 package com.example.petapp;
 
 import android.os.Bundle;
-
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 import java.util.ArrayList;
 
-
 public class CadastroPetActivity extends AppCompatActivity {
-    ArrayList<Pet> listapet ; // criei a variavel
+
+    ArrayList<Pet> listaDePet;
+    RepositorioPet repositorioPet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cadastro_pet);
-        setTitle("Cadastro Pet");
-         listapet =
-                (ArrayList<Pet>) getIntent()
-                .getSerializableExtra("lista_pet");
-        Log.i("pet", "Carregado Cadastro Pet com sucesso");
+        setTitle("Cadastro");
+
+        listaDePet  = (ArrayList<Pet>) getIntent().getSerializableExtra("lista_pet");
+        Log.i("Pet","Cadastro carregado com sucesso");
+
+        repositorioPet = new RepositorioPet(this);
+
     }
 
-    public void cadastrar(View view){
-        EditText editTextNome =
-                findViewById(R.id.editTextNomePet);
-        EditText editTextIdade =
-                findViewById(R.id.editTextIdadePet);
-        String nome = editTextNome.getText().toString();
-        String idade = editTextIdade.getText().toString();
-        if(nome.isEmpty() || idade.isEmpty()){
-            Toast.makeText(this,"Preencha os campos",
-                    Toast.LENGTH_LONG).show();
+    public void cadastrar(View view) {
+        EditText editTextNomePet = findViewById(R.id.editTextNomePet);
+        EditText editTextIdadePet = findViewById(R.id.editTextIdadePet);
+
+        String nome = editTextNomePet.getText().toString();
+        String idade = editTextIdadePet.getText().toString();
+
+        if (nome.isEmpty() || idade.isEmpty()){
+            Toast.makeText(this, "Preenche os campos", Toast.LENGTH_SHORT).show();
             return;
         }
-        Pet pet = new Pet();
-        pet.id = DadosCompartilhados.getProximoId();
-        pet.nome = nome;
-        pet.idade = Integer.parseInt(idade);
-        DadosCompartilhados.lista.add(pet);
-        Toast.makeText(this,"Cadastro realizado" +
-                        " com sucesso.",
-                Toast.LENGTH_LONG).show();
-        // limpa a tela.
-        editTextNome.setText("");
-        editTextIdade.setText("");
+        else {
+            Pet pet = new Pet();
+            pet.nome = nome;
+            pet.idade = Integer.parseInt(idade);
+            repositorioPet.adcionarPet(pet);
+            Toast.makeText(this, "Cadastro realizado com sucesso", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
-
-
 }
